2be3783ce6bb6911c9eaa67a90aab2138806e8ba
annapetrovska <petrovskaya.am@gmail.com>
2018-01-30 13:36:45 +0200

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         int result = new Practice1().sum(2, 3);
-        System.out.println(result);
+        System.out.println(result + "!");
     }
 }
